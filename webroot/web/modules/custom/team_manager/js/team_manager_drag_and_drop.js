2586a798dd1f7751918f02e2bb479bc7131604ec
Vinith Chandrakumar <chandrakumar@atlantiq.ch>
2024-05-21 18:09:39 +0200

    Änderung von Freitag
    Verfeinerungen und Code gesäubert

--- webroot/web/modules/custom/team_manager/js/team_manager_drag_and_drop.js
+++ webroot/web/modules/custom/team_manager/js/team_manager_drag_and_drop.js
@@ -1,30 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
-  console.log('test1');
   const container = document.querySelector('.view-content.gin-layer-wrapper');
 
+  //Event-Listener für Drag-Over-Ereignis
   container.addEventListener('dragover', function (e) {
     e.preventDefault();
     const draggingElement = document.querySelector('.dragging');
     const currentElement = e.target;
 
+    // Überprüfen, ob das aktuelle Element ein gültiges Ziel ist
     if (draggingElement && currentElement !== draggingElement && currentElement.classList.contains('views-row')) {
       const nextElement = (currentElement === draggingElement.nextElementSibling) ? currentElement.nextElementSibling : currentElement;
       container.insertBefore(draggingElement, nextElement);
     }
   });
-  console.log('test2');
+
+  // Event-Listener für Drop-Ereignis
   container.addEventListener('drop', function (e) {
     e.preventDefault();
     updateOrder();
   });
-  console.log('test3');
+
+  // Funktion zum Aktualisieren der Reihenfolge der Elemente
   function updateOrder() {
     const items = Array.from(container.querySelectorAll('.views-row')).map((item, index) => ({
       id: item.dataset.id, // ID des Elements
       weight: index // Indexposition als Gewicht
     }));
 
-    // AJAX-Anfrage senden
+    // AJAX-Anfrage zum Aktualisieren der Gewichte
     fetch('/team-manager/update-weights', {
       method: 'POST',
       headers: {
@@ -33,7 +36,6 @@ document.addEventListener('DOMContentLoaded', function () {
       body: JSON.stringify({ items })
     })
       .then(response => response.json())
-      .then(data => console.log(data));
-    console.log('test4');
+      .then(data => console.log(data)); // Loggt die Antwort des Servers in der Konsole
   }
 });
